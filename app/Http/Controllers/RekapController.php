801ab369f43799b199199e7eae70b23ffe44321a
SyahidanAS <syahidan139@gmail.com>
2022-06-22 11:19:13 +0700

    hotfix: cleaning rekap piutang code

--- app/Http/Controllers/RekapController.php
+++ app/Http/Controllers/RekapController.php
@@ -9,61 +9,20 @@ use Illuminate\Support\Facades\DB;
 
 class RekapController extends Controller
 {
+    // Function untuk get rekapitulasi piutang sebelum dilakukan filtering
     public function rekap_piutang(){
         $debitur = DebiturModel::get();
         $flag = "before-search";
         return view('rekap.piutang', compact('flag','debitur'));
     }
 
-
-    public function rekap_umur_piutang(){
-        $flag="after-search";
-        $tahun = date("Y");
-        $umur = DB::select("
-        SELECT piutang.no_invoice, debitur.nm_debitur, pembayaran.total_tagihan-pembayaran.total_pembayaran AS nominal_piutang, IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0, 0,DATEDIFF(now(),piutang.tgl_tempo)) AS umur_piutang,
-        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
-        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
-            IF(DATEDIFF(now(),piutang.tgl_tempo) >= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
-            IF(DATEDIFF(now(),piutang.tgl_tempo) <= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
-                IF(DATEDIFF(now(),piutang.tgl_tempo) >= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
-                IF(DATEDIFF(now(),piutang.tgl_tempo) <= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
-                    IF(DATEDIFF(now(),piutang.tgl_tempo) >= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100) )))))) AS hasil_persentase
-        FROM piutang JOIN debitur ON piutang.id_debitur=debitur.id JOIN pembayaran ON piutang.id=pembayaran.id_piutang WHERE piutang.status_piutang='Belum Lunas' AND YEAR(piutang.tgl_tempo)=$tahun;
-        ");
-        return view('rekap.umur', compact('flag','tahun','umur'));
-    }
-
-    public function rekapUmurPiutangAfter(Request $request){
-        $flag="after-search";
-        $tahun = $request->tahun;
-        $umur = DB::select("
-        SELECT piutang.no_invoice, debitur.nm_debitur, pembayaran.total_tagihan-pembayaran.total_pembayaran AS nominal_piutang, IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0, 0,DATEDIFF(now(),piutang.tgl_tempo)) AS umur_piutang,
-        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
-        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
-            IF(DATEDIFF(now(),piutang.tgl_tempo) >= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
-            IF(DATEDIFF(now(),piutang.tgl_tempo) <= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
-                IF(DATEDIFF(now(),piutang.tgl_tempo) >= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
-                IF(DATEDIFF(now(),piutang.tgl_tempo) <= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
-                    IF(DATEDIFF(now(),piutang.tgl_tempo) >= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100) )))))) AS hasil_persentase
-        FROM piutang JOIN debitur ON piutang.id_debitur=debitur.id JOIN pembayaran ON piutang.id=pembayaran.id_piutang WHERE piutang.status_piutang='Belum Lunas' AND YEAR(piutang.tgl_tempo)=$request->tahun;
-        ");
-        return view('rekap.umur', compact('flag','tahun','umur'));
-    }
-
-
-
-
-
-
-
-    public function getRekapPiutang(Request $request){
+     // Function untuk get rekapitulasi piutang setelah dilakukan filtering
+     public function getRekapPiutang(Request $request){
         $debiturs = DebiturModel::get();
         $flag = "after-search";
         $debitur = $request->debiturId;
         $from = $request->from;
         $to = $request->to;
-
-
 
         if($debitur[0] == "all"){
             $queryResult = DetailPembayaranModel::selectRaw("piutang.no_invoice, debitur.nm_debitur,pembayaran.total_tagihan, detail_pembayaran.tgl_pembayaran,detail_pembayaran.total_pembayaran AS total_pembayaran, detail_pembayaran.sisa_tagihan-detail_pembayaran.total_pembayaran AS sisa_piutang")
@@ -83,9 +42,41 @@ class RekapController extends Controller
             ->orderBy('piutang.id')
             ->get();
         }
-
-
-
         return view('rekap.piutang', compact('flag','queryResult','debiturs', 'from','to'));
+    }
+
+    //Function untuk get rekapitulasi umur piutang sebelum dilakukan filtering
+    public function rekap_umur_piutang(){
+        $flag="before-search";
+        $tahun = date("Y");
+        $umur = DB::select("
+        SELECT piutang.no_invoice, debitur.nm_debitur, pembayaran.total_tagihan-pembayaran.total_pembayaran AS nominal_piutang, IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0, 0,DATEDIFF(now(),piutang.tgl_tempo)) AS umur_piutang,
+        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
+        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
+            IF(DATEDIFF(now(),piutang.tgl_tempo) >= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
+            IF(DATEDIFF(now(),piutang.tgl_tempo) <= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
+                IF(DATEDIFF(now(),piutang.tgl_tempo) >= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
+                IF(DATEDIFF(now(),piutang.tgl_tempo) <= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
+                    IF(DATEDIFF(now(),piutang.tgl_tempo) >= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100) )))))) AS hasil_persentase
+        FROM piutang JOIN debitur ON piutang.id_debitur=debitur.id JOIN pembayaran ON piutang.id=pembayaran.id_piutang WHERE piutang.status_piutang='Belum Lunas' AND YEAR(piutang.tgl_tempo)=$tahun;
+        ");
+        return view('rekap.umur', compact('flag','tahun','umur'));
+    }
+    //Function untuk get rekapitulasi umur piutang setelah dilakukan filtering
+    public function rekapUmurPiutangAfter(Request $request){
+        $flag="after-search";
+        $tahun = $request->tahun;
+        $umur = DB::select("
+        SELECT piutang.no_invoice, debitur.nm_debitur, pembayaran.total_tagihan-pembayaran.total_pembayaran AS nominal_piutang, IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0, 0,DATEDIFF(now(),piutang.tgl_tempo)) AS umur_piutang,
+        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 0,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
+        IF(DATEDIFF(now(),piutang.tgl_tempo) <= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*5/100,
+            IF(DATEDIFF(now(),piutang.tgl_tempo) >= 30,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
+            IF(DATEDIFF(now(),piutang.tgl_tempo) <= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*10/100,
+                IF(DATEDIFF(now(),piutang.tgl_tempo) >= 60,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
+                IF(DATEDIFF(now(),piutang.tgl_tempo) <= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*50/100,
+                    IF(DATEDIFF(now(),piutang.tgl_tempo) >= 90,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100,(pembayaran.total_tagihan-pembayaran.total_pembayaran)*100/100) )))))) AS hasil_persentase
+        FROM piutang JOIN debitur ON piutang.id_debitur=debitur.id JOIN pembayaran ON piutang.id=pembayaran.id_piutang WHERE piutang.status_piutang='Belum Lunas' AND YEAR(piutang.tgl_tempo)=$request->tahun;
+        ");
+        return view('rekap.umur', compact('flag','tahun','umur'));
     }
 }
