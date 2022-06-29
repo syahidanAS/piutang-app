346f3e38b3a01b52c2585f8198d5dcd263b0ff04
SyahidanAS <syahidan139@gmail.com>
2022-06-29 14:08:33 +0700

    style: redesign login and registration form

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -6,217 +6,77 @@
     <title>{{ $title }} - PIUTANG RSKP</title>
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
-</head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"></head>
 
 <body>
-    @if (session()->has('success'))
-    <div class="sufee-alert alert with-close alert-success alert-dismissible fade show">
-        {{ session('success') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @endif
-
-    @if (session()->has('unauthorized'))
-    <div class="sufee-alert alert with-close alert-danger alert-dismissible fade show">
-        {{ session('unauthorized') }}
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    @endif
-
     {{-- LOGIN SECTION --}}
-    <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
-        <div class="card card0 border-0">
-            <div class="row d-flex">
-                <div class="col-lg-6">
-                    <div class="card1 pb-5">
-                        <div class="row">
-                            <img src="{{ asset('images/hospital-logo.png') }}" class="logo" style="width:100px; height:70px;">
-                        </div>
-                        <div class="row px-3 justify-content-center mt-4 mb-5 border-line">
-                            <img src="{{ asset('images/Invoice-pana.svg') }}" class="image">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="card2 card border-0 px-4 py-5">
-                        <form action="/login" method="POST">
-                        @csrf
-                        <div class="row px-3" style="margin-top:20%;">
-                            <label class="mb-1"><h6 class="mb-0 text-sm">Username</h6></label>
-                            <input class="mb-4" type="text" name="username" required>
-                        </div>
-                        <div class="row px-3">
-                            <label class="mb-1"><h6 class="mb-0 text-sm">Password</h6></label>
-                            <input type="password" name="password" required>
-                        </div>
-
-                        <div class="row mb-3 px-3 mt-2">
-                            <button type="submit" class="btn btn-blue text-center">Login</button>
-                        </div>
-                        <div class="row mb-4 px-3">
-                            <small class="font-weight-bold">Belum Memiliki akun? <a class="text-danger" href="/registrasi">Daftar</a></small>
-                        </div>
-                    </form>
-
-
-                    </div>
-                </div>
-            </div>
-            <div class="bg-blue py-4">
-                <div class="row px-3">
-                    <small class="ml-4 ml-sm-5 mb-2">AKU TEH BINGUNG UI LOGINNYA MAU KAYAK GIMANA :')</small>
-                    <div class="social-contact ml-4 ml-sm-auto">
-                        <span class="fa fa-facebook mr-4 text-sm"></span>
-                        <span class="fa fa-google-plus mr-4 text-sm"></span>
-                        <span class="fa fa-linkedin mr-4 text-sm"></span>
-                        <span class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span>
-                    </div>
-                </div>
-            </div>
+    <section class="vh-100">
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <img src="{{ asset('images/hospital.svg') }}"
+          class="img-fluid" alt="Hospital image">
+      </div>
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+        @if (session()->has('success'))
+        <div class="sufee-alert alert with-close alert-success alert-dismissible fade show">
+            {{ session('success') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
+        @endif
+
+        @if (session()->has('unauthorized'))
+        <div class="sufee-alert alert with-close alert-danger alert-dismissible fade show">
+            {{ session('unauthorized') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
+        <h5 class="pb-2" style="text-align: center">SISTEM PENGELOLAAN PIUTANG RUMAH SAKIT</h5>
+        <form action="/login" method="POST">
+            @csrf
+          <!-- Email input -->
+          <div class="form-outline mb-4">
+            <input type="text" id="form1Example13" class="form-control form-control-md" name="username" required/>
+            <label class="form-label" for="form1Example13">Username</label>
+          </div>
+
+          <!-- Password input -->
+          <div class="form-outline mb-4">
+            <input type="password" id="form1Example23" class="form-control form-control-md" name="password" required/>
+            <label class="form-label" for="form1Example23">Password</label>
+          </div>
+
+          <!-- Submit button -->
+          <button type="submit" class="btn btn-primary btn-md btn-block">Login</button>
+          <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0 text-muted">ATAU</p>
+          </div>
+
+          <a class="btn btn-success btn-md btn-block" href="/registrasi">Daftar Akun Baru?</a>
+        </form>
+      </div>
     </div>
+  </div>
+</section>
     {{-- END OF LOGIN SECTION --}}
     <style>
-        body {
-    color: #000;
-    overflow-x: hidden;
-    height: 100%;
-    background-color: #B0BEC5;
-    background-repeat: no-repeat;
-}
-
-.card0 {
-    box-shadow: 0px 4px 8px 0px #757575;
-    border-radius: 0px;
-}
-
-.card2 {
-    margin: 0px 40px;
-}
-
-.logo {
-    width: 200px;
-    height: 100px;
-    margin-top: 20px;
-    margin-left: 35px;
-}
-
-.image {
-    width: 360px;
-    height: 280px;
-}
-
-.border-line {
-    border-right: 1px solid #EEEEEE;
-}
-
-
-
-
-.or {
-    width: 10%;
-    font-weight: bold;
-}
-
-.text-sm {
-    font-size: 14px !important;
-}
-
-::placeholder {
-    color: #BDBDBD;
-    opacity: 1;
-    font-weight: 300
-}
-
-:-ms-input-placeholder {
-    color: #BDBDBD;
-    font-weight: 300
-}
-
-::-ms-input-placeholder {
-    color: #BDBDBD;
-    font-weight: 300
-}
-
-input, textarea {
-    padding: 10px 12px 10px 12px;
-    border: 1px solid lightgrey;
-    border-radius: 2px;
-    margin-bottom: 5px;
-    margin-top: 2px;
-    width: 100%;
-    box-sizing: border-box;
-    color: #2C3E50;
-    font-size: 14px;
-    letter-spacing: 1px;
-}
-
-input:focus, textarea:focus {
-    -moz-box-shadow: none !important;
-    -webkit-box-shadow: none !important;
-    box-shadow: none !important;
-    border: 1px solid #304FFE;
-    outline-width: 0;
-}
-
-button:focus {
-    -moz-box-shadow: none !important;
-    -webkit-box-shadow: none !important;
-    box-shadow: none !important;
-    outline-width: 0;
-}
-
-a {
-    color: inherit;
-    cursor: pointer;
-}
-
-.btn-blue {
-    background-color: #0275d8;
-    width: 150px;
-    color: #fff;
-    border-radius: 2px;
-}
-
-.btn-blue:hover {
-    background-color: #055ba7;
-    cursor: pointer;
-}
-
-.bg-blue {
-    color: #fff;
-    background-color: #0275d8;
-}
-
-@media screen and (max-width: 991px) {
-    .logo {
-        margin-left: 0px;
-    }
-
-    .image {
-        width: 300px;
-        height: 220px;
-    }
-
-    .border-line {
-        border-right: none;
-    }
-
-    .card2 {
-        border-top: 1px solid #EEEEEE !important;
-        margin: 0px 15px;
-    }
-}
+        *{
+            background-color: rgb(247, 247, 247);
+        }
+        .divider:after,
+        .divider:before {
+        content: "";
+        flex: 1;
+        height: 1px;
+        background: #eee;
+        }
     </style>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 </body>
 
 </html>
